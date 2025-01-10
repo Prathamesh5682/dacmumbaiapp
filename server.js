7ea90d0db365a33c8ea3a29f7491f7d592990647
Prathamesh Patil <prathameshpatil@9803.com>
2025-01-10 23:32:50 +0530

    first draft of web app

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var session = require('express-session')
+var parseurl = require('parseurl')
+var path = require('path')
+var bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+var sessionOptions = {
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: false
+}
+app.use(session(sessionOptions));
+
+app.use(function(req, res, next){
+  if(!req.session.views){
+    req.session.views = {};
+    req.session.views.ShoppingCart=[];
+  }
+
+  var pathname = parseurl(req).pathname;
+  //count the views
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
+  next();
+})
+
+var staticFolder=express.static(path.join(__dirname,"public"));
+app.use(staticFolder);
+
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname + "index.html"));
+});
+
+app.listen(8000);
+console.log("ShoppingCart Web App is listening on port 8000");
+
+module.exports = app;
